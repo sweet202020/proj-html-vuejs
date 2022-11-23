e441205ebb58d9af43dc4e7de86e310dd31bead2
sweet202020 <dolcealessio1@gmail.com>
2022-11-23 10:25:36 +0100

    add static map

--- src/components/LiveDates.vue
+++ src/components/LiveDates.vue
@@ -8,15 +8,50 @@ export default {
     <section class="live_dates">
         <h2>Live Dates</h2>
         <hr>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum odit enim, nobis consectetur ut, voluptate
+        <p class="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum odit enim, nobis consectetur ut,
+            voluptate
             pariatur sit qui asperiores modi dolor quasi eum blanditiis porro.
         </p>
         <div class="tour">
-            <div> <span>+ </span> 17/08/2020 gem festival 2020 anakalia, georgia</div>
-            <div><span>+ </span> 24/09/2020 groovfest dominical republic</div>
-            <div><span>+ </span> 31/10/2020 oasis festival 2020 marrakech, morocco</div>
-            <div><span>+ </span> 07/11/2020 moga festival - essaquiria, morocco</div>
-            <div><span>+ </span> 10/12/2020 enivision - uvita, costa rica</div>
+            <ul>
+                <li>
+
+                    <h6> <span>+ </span>17/08/2020 gem festival 2020 anakalia, georgia</h6>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4">
+                                <img src="../assets/img/anaklia.jpeg" alt="">
+                            </div>
+                            <!-- ./col-4 -->
+                            <div class="col-8">
+                                <h5>Untold Stories</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto unde eius labore
+                                    veniam,
+                                    beatae voluptas quasi perferendis consectetur eaque excepturi similique! Asperiores,
+                                    doloribus accusamus ullam quisquam sequi tempora quod voluptate laborum, odio, iste
+                                    rerum quibusdam.
+                                </p>
+                                <button>book now</button>
+                            </div>
+                            <!-- ./col-8 -->
+                        </div>
+                        <!-- ./row -->
+                    </div>
+                    <!-- ./container -->
+                </li>
+                <li><span>+ </span>
+                    <h6>24/09/2020 groovfest dominical republic</h6>
+                </li>
+                <li><span>+ </span>
+                    <h6>31/10/2020 oasis festival 2020 marrakech, morocco</h6>
+                </li>
+                <li><span>+ </span>
+                    <h6> 07/11/2020 moga festival - essaquiria, morocco</h6>
+                </li>
+                <li><span>+ </span>
+                    <h6>10/12/2020 enivision - uvita, costa rica</h6>
+                </li>
+            </ul>
         </div>
         <!-- /.tour -->
         <div class="view text-uppercase text-center">
@@ -33,7 +68,7 @@ export default {
     padding-top: 5rem;
     background-color: $primary-dark;
 
-    p {
+    p.title {
         padding: 3rem 0;
         color: $secondary-dark;
         text-align: center;
@@ -46,18 +81,70 @@ export default {
         width: 60%;
         margin: 3rem auto;
 
-        div {
+        ul {
+            list-style: none;
             display: flex;
+            flex-direction: column;
             align-items: flex-start;
-            background-color: $life;
             color: $primary-light;
-            margin: 1rem 0;
-            padding: 0 1rem;
-            text-transform: uppercase;
+
+            li {
+                display: flex;
+                flex-direction: column;
+                background-color: #323844;
+                width: 100%;
+                padding: 0 1rem;
+                margin: 1rem 0;
+
+                h6 {
+                    text-transform: uppercase;
+                    margin-top: 5px;
+                    align-self: flex-start;
+                }
+
+                h5 {
+                    color: $lighter;
+                    font-size: 13px;
+                }
+
+                img {
+                    max-width: 100%;
+                    object-fit: contain;
+                }
+
+                p {
+                    color: $secondary-dark;
+                    text-align: left;
+                    font-size: 13px;
+                }
+
+                .col-4 {
+                    align-self: center;
+                    margin: 1rem 0;
+                }
+
+                .col-8 {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: flex-start;
+                    margin: 1.5rem 0;
+
+                }
+
+                button {
+                    background-color: $primary-light;
+                    border: none;
+                    color: $lighter;
+                    padding: 0.5rem 0.8rem;
+                    font-size: 12px;
+                    text-transform: uppercase;
+                }
+            }
 
             span {
                 color: $lighter;
                 margin-right: 1rem;
+                margin-top: 2px;
             }
 
         }
